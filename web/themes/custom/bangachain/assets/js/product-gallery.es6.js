39c593b7cc69b2ae5f47693bae30495d37e74871
JMStewart00 <jmstewart00@gmail.com>
2022-01-13 08:02:17 -0500

    Bug fixes, sitemap update

--- web/themes/custom/bangachain/assets/js/product-gallery.es6.js
+++ web/themes/custom/bangachain/assets/js/product-gallery.es6.js
@@ -1,31 +1,43 @@
-(($, Drupal) => {
+(($, Drupal, once) => {
+  function init(props){
+    // Grab the current product variation ID.
+    if (props.productForm) {
+      const current_product_variation_id = props.productForm.dataset.productVariationId;
+
+      // Determine the gallery image that matches (if any)
+      if ($('#thumbnail-slider').length && $('#thumbnail-slider .splide__slide[data-variation-id*="' + current_product_variation_id +'"]').length) {
+        const $nav_item = $('#thumbnail-slider .splide__slide[data-variation-id*="' + current_product_variation_id +'"]');
+        const item_position = $nav_item.index();
+        // Trigger Splide to go to current variation image.
+        props.thumbnailSlider.go(item_position);
+      }
+    }
+  }
 
   let thumbnailSlider;
   let gallerySlider;
-
-  $(document).ready(() => {
-    // Create and mount the thumbnails slider.
-    thumbnailSlider = new Splide( '#thumbnail-slider', {
-      rewind      : true,
-      fixedWidth  : 100,
-      isNavigation: true,
-      gap         : 10,
-      focus       : 'center',
-      pagination  : false,
-      cover       : false,
-      perPage     : 4,
-      updateOnMove: true,
-      drag        : true,
-      breakpoints : {
-        '600': {
-          fixedWidth  : 66,
-        }
+  thumbnailSlider = new Splide( '#thumbnail-slider', {
+    rewind      : true,
+    fixedWidth  : 100,
+    isNavigation: true,
+    gap         : 10,
+    focus       : 'center',
+    pagination  : false,
+    cover       : false,
+    perPage     : 4,
+    updateOnMove: true,
+    drag        : true,
+    breakpoints : {
+      '600': {
+        fixedWidth  : 66,
       }
-    } )
+    }
+  } )
 
-    thumbnailSlider.on( 'active', (Slide) => {
-      const variationSelect = document.querySelector('select[name="purchased_entity[0][variation]"]');
+  thumbnailSlider.on( 'active', (Slide) => {
+    const variationSelect = document.querySelector('select[name="purchased_entity[0][variation]"]');
 
+    if (variationSelect !== null) {
       let variationId = Slide.slide.dataset.variationId;
       let opts = variationSelect.options;
 
@@ -36,42 +48,38 @@
           break;
         }
       }
-    } );
+    }
+  } );
 
-    // Create the main slider.
-    gallerySlider = new Splide( '#gallery-slider', {
-      type       : 'fade',
-      pagination : false,
-      arrows     : false,
-      cover      : false,
-      drag       : true,
-    } );
+  // Create the main slider.
+  gallerySlider = new Splide( '#gallery-slider', {
+    type       : 'fade',
+    pagination : false,
+    arrows     : false,
+    cover      : false,
+    drag       : true,
+  } );
 
-    // Set the thumbnails slider as a sync target and then call mount.
-    gallerySlider.sync( thumbnailSlider )
-    gallerySlider.mount();
-    thumbnailSlider.mount();
-  });
+  // Set the thumbnails slider as a sync target and then call mount.
+  gallerySlider.sync( thumbnailSlider )
+  gallerySlider.mount();
+  thumbnailSlider.mount();
 
   // Product Image Slider Ajax.
   Drupal.behaviors.productSlider = {
-    attach: (context, settings) => {
-      // Commerce add to cart form.
-      $('form.commerce-order-item-add-to-cart-form', context).once('productSlider').each((i, item) => {
-        // Grab the current product variation ID.
-        const current_product_variation_id = $(item).data('product-variation-id');
+    attach: (context) => {
+      const productForm = once(
+        'form',
+        `form.commerce-order-item-add-to-cart-form`,
+        context,
+      ).shift();
 
-        // Determine the gallery image that matches (if any)
-        if ($('#thumbnail-slider').length && $('#thumbnail-slider .splide__slide[data-variation-id*="' + current_product_variation_id +'"]').length) {
-          const $nav_item = $('#thumbnail-slider .splide__slide[data-variation-id*="' + current_product_variation_id +'"]');
-          const item_position = $nav_item.index();
-
-          // Trigger Splide to go to current variation image.
-          thumbnailSlider.go(item_position);
-        }
-
+      init({
+        productForm,
+        gallerySlider,
+        thumbnailSlider,
       });
     }
   };
 
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);
--- web/themes/custom/bangachain/assets/js/product-gallery.js
+++ web/themes/custom/bangachain/assets/js/product-gallery.js
@@ -1,29 +1,43 @@
 "use strict";
 
-(function ($, Drupal) {
+(function ($, Drupal, once) {
+  function init(props) {
+    // Grab the current product variation ID.
+    if (props.productForm) {
+      var current_product_variation_id = props.productForm.dataset.productVariationId; // Determine the gallery image that matches (if any)
+
+      if ($('#thumbnail-slider').length && $('#thumbnail-slider .splide__slide[data-variation-id*="' + current_product_variation_id + '"]').length) {
+        var $nav_item = $('#thumbnail-slider .splide__slide[data-variation-id*="' + current_product_variation_id + '"]');
+        var item_position = $nav_item.index(); // Trigger Splide to go to current variation image.
+
+        props.thumbnailSlider.go(item_position);
+      }
+    }
+  }
+
   var thumbnailSlider;
   var gallerySlider;
-  $(document).ready(function () {
-    // Create and mount the thumbnails slider.
-    thumbnailSlider = new Splide('#thumbnail-slider', {
-      rewind: true,
-      fixedWidth: 100,
-      isNavigation: true,
-      gap: 10,
-      focus: 'center',
-      pagination: false,
-      cover: false,
-      perPage: 4,
-      updateOnMove: true,
-      drag: true,
-      breakpoints: {
-        '600': {
-          fixedWidth: 66
-        }
+  thumbnailSlider = new Splide('#thumbnail-slider', {
+    rewind: true,
+    fixedWidth: 100,
+    isNavigation: true,
+    gap: 10,
+    focus: 'center',
+    pagination: false,
+    cover: false,
+    perPage: 4,
+    updateOnMove: true,
+    drag: true,
+    breakpoints: {
+      '600': {
+        fixedWidth: 66
       }
-    });
-    thumbnailSlider.on('active', function (Slide) {
-      var variationSelect = document.querySelector('select[name="purchased_entity[0][variation]"]');
+    }
+  });
+  thumbnailSlider.on('active', function (Slide) {
+    var variationSelect = document.querySelector('select[name="purchased_entity[0][variation]"]');
+
+    if (variationSelect !== null) {
       var variationId = Slide.slide.dataset.variationId;
       var opts = variationSelect.options;
 
@@ -34,35 +48,29 @@
           break;
         }
       }
-    }); // Create the main slider.
+    }
+  }); // Create the main slider.
 
-    gallerySlider = new Splide('#gallery-slider', {
-      type: 'fade',
-      pagination: false,
-      arrows: false,
-      cover: false,
-      drag: true
-    }); // Set the thumbnails slider as a sync target and then call mount.
+  gallerySlider = new Splide('#gallery-slider', {
+    type: 'fade',
+    pagination: false,
+    arrows: false,
+    cover: false,
+    drag: true
+  }); // Set the thumbnails slider as a sync target and then call mount.
 
-    gallerySlider.sync(thumbnailSlider);
-    gallerySlider.mount();
-    thumbnailSlider.mount();
-  }); // Product Image Slider Ajax.
+  gallerySlider.sync(thumbnailSlider);
+  gallerySlider.mount();
+  thumbnailSlider.mount(); // Product Image Slider Ajax.
 
   Drupal.behaviors.productSlider = {
-    attach: function attach(context, settings) {
-      // Commerce add to cart form.
-      $('form.commerce-order-item-add-to-cart-form', context).once('productSlider').each(function (i, item) {
-        // Grab the current product variation ID.
-        var current_product_variation_id = $(item).data('product-variation-id'); // Determine the gallery image that matches (if any)
-
-        if ($('#thumbnail-slider').length && $('#thumbnail-slider .splide__slide[data-variation-id*="' + current_product_variation_id + '"]').length) {
-          var $nav_item = $('#thumbnail-slider .splide__slide[data-variation-id*="' + current_product_variation_id + '"]');
-          var item_position = $nav_item.index(); // Trigger Splide to go to current variation image.
-
-          thumbnailSlider.go(item_position);
-        }
+    attach: function attach(context) {
+      var productForm = once('form', "form.commerce-order-item-add-to-cart-form", context).shift();
+      init({
+        productForm: productForm,
+        gallerySlider: gallerySlider,
+        thumbnailSlider: thumbnailSlider
       });
     }
   };
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);

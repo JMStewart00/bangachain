99f6e0dd908205742edda7aad0a0bd171fc6425d
JMStewart00 <jmstewart00@gmail.com>
2022-05-04 12:43:39 -0400

    HOTFIX for POS issue

--- web/modules/custom/bangachain_commerce_mail/src/Plugin/Commerce/EmailEvent/OrderStateUpdate.php
+++ web/modules/custom/bangachain_commerce_mail/src/Plugin/Commerce/EmailEvent/OrderStateUpdate.php
@@ -2,28 +2,28 @@
 
 namespace Drupal\bangachain_commerce_mail\Plugin\Commerce\EmailEvent;
 
-use Drupal\commerce_email\Plugin\Commerce\EmailEvent\EmailEventBase;
-use Drupal\state_machine\Event\WorkflowTransitionEvent;
-use Symfony\Component\EventDispatcher\Event;
+// use Drupal\commerce_email\Plugin\Commerce\EmailEvent\EmailEventBase;
+// use Drupal\state_machine\Event\WorkflowTransitionEvent;
+// use Symfony\Component\EventDispatcher\Event;
 
-/**
- * Provides the OrderStateUpdate email event.
- *
- * @CommerceEmailEvent(
- *   id = "order_status_updated",
- *   label = @Translation("Order State Updated"),
- *   event_name = "commerce_order.fulfill.post_transition",
- *   entity_type = "commerce_order",
- * )
- */
-class OrderStateUpdate extends EmailEventBase {
+// /**
+//  * Provides the OrderStateUpdate email event.
+//  *
+//  * @CommerceEmailEvent(
+//  *   id = "order_status_updated",
+//  *   label = @Translation("Order State Updated"),
+//  *   event_name = "commerce_order.fulfill.post_transition",
+//  *   entity_type = "commerce_order",
+//  * )
+//  */
+// class OrderStateUpdate extends EmailEventBase {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function extractEntityFromEvent(Event $event) {
-    assert($event instanceof WorkflowTransitionEvent);
-    return $event->getEntity();
-  }
+//   /**
+//    * {@inheritdoc}
+//    */
+//   public function extractEntityFromEvent(Event $event) {
+//     assert($event instanceof WorkflowTransitionEvent);
+//     return $event->getEntity();
+//   }
 
-}
+// }
